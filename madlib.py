9cfafff82219c7cc46ab5454efa45b11a6b66719
Princewill <princewillfidelis1@gmail.com>
2022-09-09 16:26:22 -0700

    initial commit

---
+++ madlib.py
@@ -0,0 +1,45 @@
+# Words to be inputted by player
+words = [
+    input("Adjective: "),
+    input("Adjective: "),
+    input("Noun: "),
+    input("Noun: "),
+    input("Adjective: "),
+    input("Noun: "),
+    input("Plural Noun: "),
+    input("Adjective: "),
+    input("Number: "),
+    input("Color: "),
+    input("Number: "),
+    input("Plural Noun: "),
+    input("Adjective: "),
+    input("A Place: "),
+    input("Color: "),
+    input("Plural Noun: "),
+    input("Verb: ")
+]
+
+# Story of the mad-libs Game
+print()
+print("------------------------------------------------")
+print("A PALACE FIT FOR A HEDGEHOG")
+print()
+print(f"Sonics abode is a very {words[0]} place.")
+print(f"At first glance, it seems like a/an {words[1]} cave.")
+print(f"But if you look closer, you'll see it's actually a comfortable {words[2]}.")
+print(f"For one, Sonic has a super-cozy beanbag {words[3]}.")
+print(f"When he feels like listening to some {words[4]} tune from the 1980s,")
+print(f"Sonic turns on his old school {words[5]}, ")
+print(f"pulls out his collection of {words[6]}")
+print(f"In addition to great tunes,")
+print(f"Sonic has plenty of {words[7]} equipment to keep him busy.")
+print(f"There's a dryer that Sonic uses as a treadmill to run {words[8]} miles a day.")
+print(f"He has a/an {words[9]} ping-pong table where he play against...himself.")
+print(f"(His record is {words[10]} wins and zero losses!) and for that final touch,")
+print(f"Sonic hung {words[11]} all over his cave walls.")
+print(f"He's got a/an {words[12]} photo of (the) {words[13]} next to")
+print(f"a/an {words[14]} poster of {words[15]}. It may not be much,")
+print(f"but Sonic loves to {words[16]} in his cave all the time!")
+print()
+print("------------------------------------------------")
+
